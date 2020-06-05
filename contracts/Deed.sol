00b70cc2b5a8c7b0f1253c2c60cd6d6d7bf53993
Kyle Phillips <khillips375@yahoo.com>
2020-06-04 20:36:58 -0400

    spelling error fixed

--- contracts/Deed.sol
+++ contracts/Deed.sol
@@ -1,24 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract Deed {
-    address public lawyer;
-    address payable public benificiary;
-    uint public earliest;
+  address public lawyer;
+  address payable public beneficiary;
+  uint public earliest;
 
-    constructor(
-        address _lawyer,
-        address payable _beneficiary,
-        uint fromNow
-    )
-    payable
+  constructor(
+    address _lawyer,
+    address payable _beneficiary,
+    uint fromNow)
+    payable 
     public {
-        laywer = _lawyer;
-        beneficiary = _beneficiary;
-        earliest = now + fromNow
-    }
-    function withdraw() public {
-        require(msg.sender == lawyer, 'lawyer only');
-        require(now >= earliest, 'too early');
-        beneficiary.transfer(address(this).balance);
-    }
+    lawyer = _lawyer;
+    beneficiary = _beneficiary;
+    earliest = now + fromNow;
+  }
+
+  function withdraw() public {
+    require(msg.sender == lawyer, 'lawyer only');
+    require(now >= earliest, 'too early');
+    beneficiary.transfer(address(this).balance);
+  }
 }
