9e85fd5588e565c4d08f96528107d000919682d8
Kyle Phillips <khillips375@yahoo.com>
2020-06-08 14:12:17 -0400

    Home screen setup

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="main-home">
+            <h1>HELLO THIS IS THE HOME SCREEN</h1>
+        </div>
+    )
+}
+
+export default Home;
