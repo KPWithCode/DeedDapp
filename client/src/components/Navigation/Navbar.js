4b1c219c282ffd495744d98fdf151df68897ab32
Kyle Phillips <khillips375@yahoo.com>
2020-06-08 14:11:08 -0400

    Navbar and styling

---
+++ client/src/components/Navigation/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Nav = props => {
+    return (
+        <div>
+            <nav className="main-nav ">
+                <div className="main-nav-logo">
+                    <h1><NavLink to="/">DeedDapp</NavLink></h1>
+                </div>
+                <nav className="main-nav-items">
+                    <ul>
+                        <li><NavLink to="/balance">Show Balance</NavLink></li>
+                        <li><NavLink to="/withdraw">Withdraw</NavLink></li>
+                            <React.Fragment>
+                                <li><NavLink to="/highlights">Highlighted</NavLink></li>                            </React.Fragment>        
+                    </ul>
+                </nav>
+            </nav>
+            <hr style={{ width: '90%' }} />
+        </div>
+    )
+}
+export default Nav;
