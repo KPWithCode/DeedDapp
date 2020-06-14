efbd6b665fde042378cdd194958c51886f1d3cef
Kyle Phillips <khillips375@yahoo.com>
2020-06-13 23:31:24 -0400

    Web3

--- client/src/pages/Balance/balance.js
+++ client/src/pages/Balance/balance.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import './balance.css'
+import './balance.css';
+import Web3 from 'web3'
+import Deed from '../../contracts/Deed.json';
+
+let web3;
+let deed;
 
 const Balance = () => {
+
+    const initWeb3 = () => {
+        return new Promise((resolve, reject) => {
+            if
+        })
+    }
     return (
         <div className="main-balance">
             <h1>Check your balance below</h1>
