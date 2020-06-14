d0429df18d4411f5a79647bfab9e4693a14a14d2
Kyle Phillips <khillips375@yahoo.com>
2020-06-14 01:52:45 -0400

    Web3 and Deed contract instance

--- client/src/pages/Balance/balance.js
+++ client/src/pages/Balance/balance.js
@@ -10,8 +10,38 @@ const Balance = () => {
 
     const initWeb3 = () => {
         return new Promise((resolve, reject) => {
-            if
-        })
+            if (typeof window.ethereum !== 'undefined') {
+                const web3 = new Web3(window.ethereum);
+                window.ethereum.enable()
+                    .then(() => {
+                        resolve(
+                            new Web3(window.ethereum)
+                        )
+                    })
+                    .catch(e => {
+                        reject(e);
+                    });
+                    return;
+            }
+            if (typeof window.web3 !== 'undefined') {
+                return resolve(
+                    new Web3(window.web3.currentProvider)
+                )
+            }
+            resolve(new Web3('http://localhost:9545'))
+        });
+    };
+    // contract instance
+    const initContract = async () => {
+        const networkId = await web3.eth.net.getId();
+        return new web3.eth.Contract(
+            // abi
+            Deed.abi,
+            // address of smart contract
+            Deed   
+                .networks[networkId]
+                .address
+        )
     }
     return (
         <div className="main-balance">
